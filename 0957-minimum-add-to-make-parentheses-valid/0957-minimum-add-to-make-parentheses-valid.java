6de0307aca5a3d034a4e07f52228927798aa6a86
DILIP KUMAR B C <57721059+DILIPKUMARBC8055@users.noreply.github.com>
2024-10-09 09:12:18 +0530

    Time: 0 ms (100%), Space: 41.2 MB (79.36%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,19 +1,33 @@
 class Solution {
 
     public int minAddToMakeValid(String s) {
-        Stack<Character> open = new Stack<>();
-        Stack<Character> close = new Stack<>();
+        // Stack<Character> open = new Stack<>();
+        // Stack<Character> close = new Stack<>();
+        // for (char c : s.toCharArray()) {
+        //     if (c == '(') {
+        //         open.push(c);
+        //     } else {
+        //         if (!open.isEmpty()) {
+        //             open.pop();
+        //         } else {
+        //             close.push(c);
+        //         }
+        //     }
+        // }
+        // return close.size() + open.size();
+        int open = 0;
+        int close = 0;
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                open.push(c);
+                open++;
             } else {
-                if (!open.isEmpty()) {
-                    open.pop();
+                if (open > 0) {
+                    open--;
                 } else {
-                    close.push(c);
+                    close++;
                 }
             }
         }
-        return close.size() + open.size();
+        return open + close;
     }
 }
