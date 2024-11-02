9b6f4ce2506b16a31d226009ccd0ee0f50f88b34
DILIP KUMAR B C <57721059+DILIPKUMARBC8055@users.noreply.github.com>
2024-11-03 00:07:29 +0530

    Time: 25 ms (100%), Space: 45.9 MB (21.27%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,25 +1,37 @@
 class Solution {
 
     public String makeFancyString(String s) {
-        char second = 'A';
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (stack.isEmpty()) {
-                stack.push(c);
-            } else if (stack.size() == 1) {
-                second = stack.peek();
-                stack.push(c);
-            } else {
-                if (!(second == stack.peek() && stack.peek() == c)) {
-                    second = stack.peek();
-                    stack.push(c);
-                }
-            }
+        // char second = 'A';
+        // Stack<Character> stack = new Stack<>();
+        // for (char c : s.toCharArray()) {
+        //     if (stack.isEmpty()) {
+        //         stack.push(c);
+        //     } else if (stack.size() == 1) {
+        //         second = stack.peek();
+        //         stack.push(c);
+        //     } else {
+        //         if (!(second == stack.peek() && stack.peek() == c)) {
+        //             second = stack.peek();
+        //             stack.push(c);
+        //         }
+        //     }
+        // }
+        // StringBuilder str = new StringBuilder();
+        // while (!stack.isEmpty()) {
+        //     str.append(stack.pop() + "");
+        // }
+        // return str.reverse().toString();
+        int sameCount=0;
+        StringBuilder sb=new StringBuilder();
+        char last=s.charAt(0);
+       
+        for(char now: s.toCharArray())
+        {
+            if(now==last) sameCount++;
+            else sameCount=1;
+            if(sameCount<3) sb.append(now);
+            last=now;
         }
-        StringBuilder str = new StringBuilder();
-        while (!stack.isEmpty()) {
-            str.append(stack.pop() + "");
-        }
-        return str.reverse().toString();
+        return sb.toString();
     }
 }
